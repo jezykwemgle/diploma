c419285a866012209d0c4d425a8d9f64dcf4af3c
jezyk_we_mgle <anastasiafabricnaama@gmail.com>
2024-02-19 15:15:26 +0200

    :bug:

--- src/authentication/forms.py
+++ src/authentication/forms.py
@@ -1,3 +1,4 @@
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
 
@@ -19,3 +20,12 @@ class RegistrationForm(UserCreationForm):
             "password1",
             "password2",
         ]
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                "The two password fields didn't match."
+            )
+        return password2
